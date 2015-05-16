16d62a08d1a0ec5824d8fe14602e139b01cf9dd9
Slardardetenis <giiovaani@gmail.com>
2015-05-16 19:11:58 -0300

    chagens at the shiny app

--- server.R
+++ server.R
@@ -1,4 +1,9 @@
 ## app.R ##
+library(shiny)
+library(datasets)
+library(agricolae)
+library(multcomp)
+
 dataset <- read.csv("/home/giovani/Área de Trabalho/carpet.csv",h=T,
                     col.names = c("durabilidade","tapete","composicao"))
 attach(dataset)
@@ -9,14 +14,10 @@ anovi <- aov(durabilidade~composicao, data=dataset)
 
 anovo <- aov(durabilidade~tapete, data=dataset)
 
+mcHSD <- glht(anovo,linfct=mcp(tapete="Tukey"))
+
+
 servrer <- function(input,output){
-  #set.seed(122)
-  histdata <- dataset$durabilidade
-  
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
-  })
   
   output$dados <- renderDataTable({dataset})
   
@@ -24,4 +25,41 @@ servrer <- function(input,output){
     summary(anovo)
   })
   
+  formulaText <- reactive({
+    paste("durabilidade~",input$variable)
+  })
+  
+  # Return the formula text for printing as a caption
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against durabilidade and only
+  # include outliers if requested
+  
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()),
+          data = dataset,
+          outline = input$outliers)
+  })
+  
+  formulaVerb <- reactive({
+    paste(input$test)
+  })
+  
+  output$caption1 <- renderText({
+    formulaVerb()
+  })
+  
+  output$anoverb <-renderPrint({
+    summary(glht(anovo,linfct=mcp(tapete=formulaVerb())))
+  })
+   
+  output$plotgg <- renderPlot({
+    plot(confint(glht(anovo,linfct=mcp(tapete=formulaVerb())),level=0.95))
+  })
+  
+#   output$group <- renderPrint({
+#     summary()
+#   })
 }
